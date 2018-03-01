978b79183b99f45dfae48f0d6a86f9aea0de30b2
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-03-01 10:43:16 -0500

    Adding demo

---
+++ demo/site/src/main/java/org/onehippo/forge/pageflow/demo/beans/EventsDocument.java
@@ -0,0 +1,112 @@
+package org.onehippo.forge.pageflow.demo.beans;
+/*
+ * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.util.Calendar;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "pageflowdemo:eventsdocument")
+@Node(jcrType="pageflowdemo:eventsdocument")
+public class EventsDocument extends HippoDocument {
+
+    /**
+     * The document type of the events document.
+     */
+    public final static String DOCUMENT_TYPE = "pageflowdemo:eventsdocument";
+
+    private final static String TITLE = "pageflowdemo:title";
+    private final static String DATE = "pageflowdemo:date";
+    private final static String INTRODUCTION = "pageflowdemo:introduction";
+    private final static String IMAGE = "pageflowdemo:image";
+    private final static String CONTENT = "pageflowdemo:content";
+    private final static String LOCATION = "pageflowdemo:location";
+    private final static String END_DATE = "pageflowdemo:enddate";
+
+    /**
+     * Get the title of the document.
+     *
+     * @return the title
+     */
+    @HippoEssentialsGenerated(internalName = "pageflowdemo:title")
+    public String getTitle() {
+        return getProperty(TITLE);
+    }
+
+    /**
+     * Get the date of the document, i.e. the start date of the event.
+     *
+     * @return the (start) date
+     */
+    @HippoEssentialsGenerated(internalName = "pageflowdemo:date")
+    public Calendar getDate() {
+        return getProperty(DATE);
+    }
+
+    /**
+     * Get the introduction of the document.
+     *
+     * @return the introduction
+     */
+    @HippoEssentialsGenerated(internalName = "pageflowdemo:introduction")
+    public String getIntroduction() {
+        return getProperty(INTRODUCTION);
+    }
+
+    /**
+     * Get the image of the document.
+     *
+     * @return the image
+     */
+    @HippoEssentialsGenerated(internalName = "pageflowdemo:image")
+    public HippoGalleryImageSet getImage() {
+        return getLinkedBean(IMAGE, HippoGalleryImageSet.class);
+    }
+
+    /**
+     * Get the main content of the document.
+     *
+     * @return the content
+     */
+    @HippoEssentialsGenerated(internalName = "pageflowdemo:content")
+    public HippoHtml getContent() {
+        return getHippoHtml(CONTENT);
+    }
+
+    /**
+     * Get the location of the document.
+     *
+     * @return the location
+     */
+    @HippoEssentialsGenerated(internalName = "pageflowdemo:location")
+    public String getLocation() {
+        return getProperty(LOCATION);
+    }
+
+    /**
+     * Get the end date of the document, i.e. the end date of the event.
+     *
+     * @return the end date
+     */
+    @HippoEssentialsGenerated(internalName = "pageflowdemo:enddate")
+    public Calendar getEndDate() {
+        return getProperty(END_DATE);
+    }
+
+}
