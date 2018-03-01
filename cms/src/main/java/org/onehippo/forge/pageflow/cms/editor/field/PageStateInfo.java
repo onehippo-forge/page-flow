59f656aa7eb136c368e8982fc8956db85c7e2d10
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-03-01 18:11:46 -0500

    Updating flow doc type

---
+++ cms/src/main/java/org/onehippo/forge/pageflow/cms/editor/field/PageStateInfo.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 2018 Hippo B.V. (http://www.onehippo.com)
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.forge.pageflow.cms.editor.field;
+
+import java.io.Serializable;
+
+public class PageStateInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String stateId;
+
+    private String name;
+
+    private String description;
+
+    public String getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(String stateId) {
+        this.stateId = stateId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
